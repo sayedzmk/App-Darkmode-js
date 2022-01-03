8ae67ef32f4de60e8eb3f5631227299472e2b0a9
sayedzmk <sayed77mohamed@gmail.com>
2022-01-03 18:34:34 +0200

    updat-validtion-input

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 let body =document.body;
-let btn =document.querySelector('button');
+let btn =document.querySelector('.mybtn');
 let h1 =document.querySelector('h1');
 let show =document.querySelector('.myclick');
-let div = document.querySelector('.div');
+let div = document.querySelector('.show');
 let remov=document.querySelector('.myclose');
 let addd= document.querySelector('.add')
 let input =document.querySelector('input');
-let parph = document.querySelector('p');
+let parph = document.querySelector('.div2');
 let del =document.querySelector('.delete');
+let taskes=document.querySelector('.taskes');
+let closeMessage=document.getElementById('closeMessage');
+let invalid=document.getElementById('invalid');
 let darkMode = ()=>{
-    if(body.classList.toggle("dark")){
+    body.classList.toggle("dark");
+    if(body.classList.contains("dark")){
         btn.classList ="mybtn btn btn-light fas fa-lightbulb";
         h1.style.color="#0080ff"
         div.style.background="gray";
@@ -21,33 +25,61 @@ let darkMode = ()=>{
     }
 }
 let action =()=>{
-    if(div.classList.toggle("none")){
-        div.style.display="block";
-    }
-    else{
-        div.style.display="none";
-    }
-}
-
-let delet =()=>{
-    div.style.display="none";
+    div.classList.toggle("none");
     input.value="";
 }
-let addpraph =()=>{
-    if(parph.classList.toggle("p")){
-        p=input.value;
-        parph.textContent=p;
-        del.style.display="block";
 
+let close =()=>{
+    div.classList.add('none');
+    input.value="";
+}
+
+let addTask =()=>{
+    let inputData=input.value;
+    if(inputData.trim() == 0 || inputData.length < 3 || inputData.length > 20){
+        invalid.classList.remove('none');
+    }else{
+        parph.classList.add('none');
+        taskes.innerHTML += `<div class=' del div3 my-4 '> ${inputData}
+        <button  class='delete float-right my-2  mx-2 btn btn-danger'> Delete </button>
+        </div>`;
     }
+    taskes.addEventListener('click', function (r) {
+        if (r.target.classList.contains('del')) {
+            r.target.classList.toggle('remov');
+        }
+    })
+    input.value="";
+    //parph.innerText=inputData;
+    // let newDiv=document.createElement('div');
+    // newDiv.classList='div3 my-4';
+    // newDiv.append(inputData);
+    // taskes.append(newDiv);
+}
+let closeMessageInvalid=()=>{
+    invalid.classList.add('none');
 }
 let delpaph =()=>{
     parph.textContent="No data Added Yet";
     del.style.display="none";
 }
-del.addEventListener('click',delpaph);
-addd.addEventListener('click',addpraph);
-remov.addEventListener('click',delet);
+let showNoTasks = () => {
+    if (taskes.childElementCount == 0) {
+        parph.classList.remove('none');
+    }
+}
+let deleteTask = (e) => {
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+        showNoTasks();
+    }
+}
+
+document.addEventListener('click', deleteTask)
+addd.addEventListener('click',addTask);
+remov.addEventListener('click',close);
 btn.addEventListener('click',darkMode);
 show.addEventListener('click',action);
+closeMessage.addEventListener('click',closeMessageInvalid)
+
 /** end */
